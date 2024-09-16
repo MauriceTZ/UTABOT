6767097a5f25675e4d180c9cb0ec0f1a5584865e
Maurice <106934566+MauriceTZ@users.noreply.github.com>
2024-09-16 01:39:30 -0300

    quedaron los script antiguos xd

--- scripts/remote_run.bat
+++ scripts/remote_run.bat
@@ -1,6 +1,9 @@
 :: PARA OBTENER LA IP DE LA RASPBERRY, PONER "hostname -I"
 :: EN LA CONSOLA DE LA RASPBERRY, RECORDAR PONER LA RASPBERRY EN EL MISMO WIFI DE LA PC,
 :: LUEGO PARA EJECUTAR ESTE SCRIPT PONER "./run.bat"
-SCP test.py utabot@192.168.72.177:/home/utabot
-@REM SSH utabot@192.168.72.179 -t "sudo killall python3; python3 test.py"
-SSH utabot@192.168.72.177 -t "sudo python3 test.py"
+
+set "RPI_ADDRESS=192.168.1.177"
+
+SCP test.py utabot@%RPI_ADDRESS%:/home/utabot
+:: REM SSH utabot@%RPI_ADDRESS% -t "sudo killall python3; python3 test.py"
+SSH utabot@%RPI_ADDRESS% -t "sudo python3 test.py"
